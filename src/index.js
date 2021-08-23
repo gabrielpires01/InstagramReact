545b39605904261c2e787d7a5d6a7e88a27f147e
Gabriel Pires <gabriel.pires@twistsystems.com>
2021-08-23 13:07:11 -0300

    NavBar added

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import ReactDOM from 'react-dom'
 import SideBar from './sidebar'
+import NavBar from './navbar'
 
 
 function App () {
     return (
         <div>
+            <NavBar />
             <SideBar />
         </div>
     )
